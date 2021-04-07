c202385cfa4816441922d8f3881759fc95e03e16
Enrique Sanchez <enrique.sanchez@docomodigital.com>
2021-04-07 11:55:00 +0200

    schedule show tournaments from database in form

--- src/main/java/es/basket/rmadrid/model/ResultsModel.java
+++ src/main/java/es/basket/rmadrid/model/ResultsModel.java
@@ -1,12 +1,25 @@
 package es.basket.rmadrid.model;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
+
+import es.basket.rmadrid.jpa.entity.Tournaments;
+import es.basket.rmadrid.jpa.repository.TournamentsRepository;
 
 @Component
 public class ResultsModel extends BaseModel implements Models<Model> {
 
+	@Autowired
+	private TournamentsRepository tournamentsRepository;
+	
 	public void execute(Model model) {
 		super.execute(model);
+		
+		List<Tournaments> tournaments = tournamentsRepository.findAll();
+	
+		model.addAttribute("tournaments", tournaments);
 	}
 }
